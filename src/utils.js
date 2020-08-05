be64b55243e47a66a6f00c27195414f7cb670b6c
Kristina Vuychich <kozetta2004@yandex.ru>
2020-08-05 20:27:20 +0300

    Импортирует моки фильтра

--- src/utils.js
+++ src/utils.js
@@ -5,19 +5,34 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
+// текущая дата
+const getCurrentDate = () => {
+const currentDate = new Date();
+currentDate.setHours(23, 59, 59, 999);
+
+return new Date(currentDate);
+};
+
 // проверяет просрочена ли задача
 export const isTaskExpired = (dueDate) => {
   if (dueDate === null) {
     return false;
   }
 
-  // текущая дата
-  let currentDate = new Date();
-  currentDate.setHours(23, 59, 59, 999);
-  currentDate = new Date(currentDate);
-
+  const currentDate = getCurrentDate();
+  
   // если текущая дата позже сгенерированной - true
   return currentDate.getTime() > dueDate.getTime();
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+    if (dueDate === null) {
+        return false
+    }
+
+    const currentDate = getCurrentDate();
+
+    return currentDate.getTime() === dueDate.getTime();
 };
 
 // берет из объекта с повтороми по дням есть/нет повтор, возвращает true/false
